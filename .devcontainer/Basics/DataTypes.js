7ab580a228d120e698ec05cbc900398c3ef884d8
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-04 18:37:43 +0000

    third lecture Done

---
+++ .devcontainer/Basics/DataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello G ") // we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("jai shree ram");
+// code readability should be high
+
+// 1.number => 2 to power 53
+// 2.bigint
+//3.string => ""
+//4.boolean => true or false
+//5.null => standalone value
+//6.undefined => declared a value but not assigned something
+//7.symbol => uniqeness find
+
+// object
+ 
+console.log(typeof(undefined)); // output undefined
+console.log(typeof(null)); // output object
+
