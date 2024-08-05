edf7635024af5b6ec87b45826fcb7f9c1ef25872
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-05 10:53:38 +0000

    number and Math Done

---
+++ .devcontainer/Basics/numberAndMaths.js
@@ -0,0 +1,48 @@
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(2))
+
+// console.log(balance.toString().length);
+
+
+const onenumber = 123.987
+// console.log(onenumber.toPrecision(3));
+// console.log('jia');
+
+const num = 100000
+// console.log(num.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++   Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-123));
+// console.log(Math.round(18.7));
+// // flor and cell 
+// flor means less than number
+// ceil means upper than number
+// console.log(Math.floor(12.2));
+// console.log(Math.ceil(12.1));
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+
+// ****************** vvi Math.random *******************
+console.log(Math.random());
+// console.log((Math.random())*10);
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+// ***************   VVi ***********************************
+
+const min  = 5
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+// ***************   VVi ***********************************
+
+
+
+
+
+
+
+
