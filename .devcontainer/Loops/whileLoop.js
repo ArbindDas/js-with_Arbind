952a730f932bbe5a08f407e401edbf49fb5c1dbf
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-06 07:36:29 +0000

    Done

---
+++ .devcontainer/Loops/whileLoop.js
@@ -0,0 +1,33 @@
+//*********************** While ********************** 
+
+// let a = 1;
+// while(a<=10){
+//     console.log(a);
+//     a++; 
+// }
+
+
+// let myarr = ["jai" , "bhim" , "biru"]
+// let arr = 0;
+// while (arr<myarr.length) {
+//     console.log(myarr[arr]);
+//     arr++;
+// }
+
+
+// const myval = ["arbind" , "sanjay" , "suraj" , "avi"]
+// let i = 0;
+// while (i<myval.length) {
+//     console.log(`value is :${myval[i]}`);
+//     i++;
+    
+// }
+
+// ********** do whileLoop ***************
+// let num = 1;
+
+// do {
+//     console.log(`number is : ${num}`);
+//     num++;
+    
+// } while (num>5);  // at least one time do while is run
