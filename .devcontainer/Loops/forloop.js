31ab05cf9222c534230d0a5e43e949194bf6b52a
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-06 06:27:58 +0000

    done

---
+++ .devcontainer/Loops/forloop.js
@@ -0,0 +1,38 @@
+// for loop
+// for(let a = 1; a<=10; a++){
+//     console.log('Numbers : '+a);
+    
+// }
+
+
+// for(let num = 1; num<=10; num++){
+//     if (num==5) {
+//         console.log("5 is the best number");
+//     }
+//     console.log("numbers : "+num);
+    
+// }
+
+// nested loop
+
+// for(let i = 1; i<=10; i++){
+//     console.log(`outer loop : ${i}`);
+    
+//     for(let j = 1; j<=10; j++){
+//     console.log(`inner loop : ${j} and outer loop ${j}` );
+//     }
+// }
+
+// printing the tabels
+// for(let i = 1; i<=10; i++){
+//     console.log('outer loop : '+i);
+//     for(let j = 1; j<=10; j++){
+//         console.log(i + '*' + j + "=" + i*j);
+//     }
+// }
+
+// loop with Array 
+// let myarr = [1,2,3,4,5]
+// for (let index = 0; index < myarr.length; index++) {
+//     console.log(myarr[index]);
+// }
