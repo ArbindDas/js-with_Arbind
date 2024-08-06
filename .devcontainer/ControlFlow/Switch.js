6ad291a96227897680136805d02f87b7919219a2
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-06 05:52:59 +0000

    Done

---
+++ .devcontainer/ControlFlow/Switch.js
@@ -0,0 +1,17 @@
+// let date = 8;
+// switch (date) {
+//     case 1:
+//         console.log('sunday');
+//         break;
+//     case 2:
+//         console.log("monday");
+//         break;
+//     case 8:
+//         console.log("thanks for using  ");
+//         break;
+        
+
+//     default:
+//         console.log('enter the valid date number');
+//         break;
+// }
