f207eded33f9433d2adcbce98b0342250e20869e
ArbindDas <160478440+ArbindDas@users.noreply.github.com>
2024-08-06 07:54:20 +0000

    Done

---
+++ .devcontainer/High_Order_Loop/forOf_loop.js
@@ -0,0 +1,51 @@
+// High Order Array loops 
+
+// let myarr = ['jai' , 'biru' , 'bhm']
+// for ( let element of myarr) {
+//     console.log(element);
+    
+// }
+
+// const arr = [1,2,3,4,5]
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+
+// const greetings = "hello World";
+// for (const greet of greetings) {
+//     if (greet === " ") {
+//         continue;
+//     }
+//     console.log(`Each Char is :${greet} `)
+// }
+
+// maps => hold only unique values
+
+const map = new Map()
+map.set('In',"india")
+map.set('In',"india")
+map.set('Ne',"Nepal")
+map.set('fr',"framce")
+// console.log(map);
+
+// for (const element of map) {
+//     console.log(element);
+    
+// }
+
+// printing key and values both
+// for (const [key , value] of map) {
+//     console.log(`key: ${key}: : values => ${value}`);
+    
+// }
+
+
+// loop with objects
+const myobj = {
+    'game1' : "NFs",
+    'game2': "SpiderMan"
+}
+// for (const [key , value] of myobj) {
+//     console.log(`key: ${key}: : values => ${value}`);
+// }  // forof not iterable on objects
